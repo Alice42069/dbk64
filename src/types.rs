143d53742992424bff174855eb12d09a2b9b0563
Alice42069 <alicepepper42069@proton.me>
2025-01-26 23:00:31 -0800

    from and default for pointer type

--- src/types.rs
+++ src/types.rs
@@ -3,9 +3,16 @@ use std::marker::PhantomData;
 use dataview::Pod;
 
 #[repr(transparent)]
+#[derive(Debug, Default, Clone, Hash, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Pointer<T: ?Sized = ()> {
     pub inner: u64,
     phantom_data: PhantomData<fn() -> T>,
 }
 
 unsafe impl<T: ?Sized + 'static> Pod for Pointer<T> {}
+
+impl<T: ?Sized> From<u64> for Pointer<T> {
+    fn from(address: u64) -> Pointer<T> {
+        Pointer { inner: address, phantom_data: PhantomData }
+    }
+}
